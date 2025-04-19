f1c235a4569a2e655a741f1910b714885bbf654c
Yushuishen <shuishenyu@gmail.com>
2025-04-19 14:46:34 +0800

    初始化第三次实验文件夹
    初始化第三次实验文件夹

--- Spring_Mybatis_Test/src/main/java/SpringBoot/Spring_Mybatis_Test/SpringMybatisTestApplication.java
+++ Spring_Mybatis_Test/src/main/java/SpringBoot/Spring_Mybatis_Test/SpringMybatisTestApplication.java
@@ -1,16 +1,13 @@
-package com.example.demo;
+package SpringBoot.Spring_Mybatis_Test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author Yu'S'hui'shen
- */
 @SpringBootApplication
-public class DemoApplication {
+public class SpringMybatisTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringMybatisTestApplication.class, args);
 	}
 
 }
--- Spring_Mybatis_Test/src/test/java/SpringBoot/Spring_Mybatis_Test/SpringMybatisTestApplicationTests.java
+++ Spring_Mybatis_Test/src/test/java/SpringBoot/Spring_Mybatis_Test/SpringMybatisTestApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package SpringBoot.Spring_Mybatis_Test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringMybatisTestApplicationTests {
 
 	@Test
 	void contextLoads() {
